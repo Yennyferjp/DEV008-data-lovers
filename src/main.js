0d3979b04689a491ea61b09c507482e403fe1a09
Yennyfer Jarava <yennyfer.jarava92@gmail.com>
2023-05-15 10:06:51 -0500

    Creación de : Barra de navegacion Estilos en CSS Funcionalidad de despliegue de icono bars (aun no funciona)

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
-import { example } from './data.js';
+// import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+// import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+// console.log(example, data);
+
+function toggleNav() {
+
+    var navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('nav-active');
+  }
