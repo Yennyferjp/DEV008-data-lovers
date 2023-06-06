bf8615223b357af84a70e8284f1c56237fbb2a8a
Yennyfer Jarava <yennyfer.jarava92@gmail.com>
2023-06-06 13:00:32 -0500

    se creó funcion en main.js para ordenar de forma ascendente los items del menú

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { obtenerCategorias, obtenerSubcategorias, busqueda, menuArreglo, filtrado } from './data.js'
+import { obtenerCategorias, obtenerSubcategorias, busqueda, menuArreglo, filtrado} from './data.js'
 
 //CONTAINER DE PLATOS
 const menuItemsContainer = document.getElementById('menu-items');
@@ -50,11 +50,11 @@ function creacionCajonCategorias() {
   espacio.setAttribute("id", "es");
 
   categorias.forEach(c => {
-    let option;
+    
 
     espacio.appendChild(select);
     section.appendChild(espacio);
-    option = document.createElement("option");
+    const option = document.createElement("option");
     option.setAttribute("value", c);
     option.innerHTML = c;
     select.appendChild(option);
@@ -132,6 +132,7 @@ verMasBtn.addEventListener('click', function () {
 // FUNCION CON LIMITE DE PRODUCTOS
 function limitRender(menu, limit) {
   //SLICE CORTA EL ARREGLO PARA QUE TRAIGA PRODUCTOS DESDE EL CERO HASTA UN LIMITE 12
+  menu = ordenarMenu(menu);
   const nextMenu = menu.slice(0, limit)
   renderMenuItems(nextMenu)
 }
@@ -182,5 +183,14 @@ fetch('testimonios.json')
 
     });
   });
+  
+
+function ordenarMenu(menuNoOrdenado) {
+  const menuClonado = [...menuNoOrdenado]
+  return menuClonado.sort(function(a, b){
+    if(a.name < b.name) { return -1; }
+    if(a.name > b.name) { return 1; }
+    return 0;}); 
+}
 ////////////////////////////////////////////////////////////////////////////////////////
 
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,53 @@
-import { example, anotherExample } from '../src/data.js';
-
-
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+import { obtenerCategorias,obtenerSubcategorias,busqueda,filtrado} from '../src/data.js';
+describe('obtenerCategorias', () => {
+  it('returna un array de categorias unicas', () => {
+    const categorias = obtenerCategorias();
+    expect(Array.isArray(categorias)).toBe(true);
+    expect(categorias.length).toBeGreaterThan(0);
   });
-
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it('verificamos que sea funcion', () => {
+    expect(typeof obtenerCategorias).toBe('function');
+  });
+  it('verifica que retorne arreglo con categorias', () => {
+    const llamadaFuncion = obtenerCategorias()
+    const categoriasEsperadas = ["Fuerte", "Entrada","Postre","Bebida"]
+    expect(JSON.stringify(llamadaFuncion)).toBe(JSON.stringify(categoriasEsperadas));
+  });
+});
+describe('obtenerSubcategorias', () => {
+  it('verificamos que sea funcion', () => {
+    expect(typeof obtenerSubcategorias).toBe('function');
+  });
+  it('returna un array de subcategorias unicas', () => {
+    const subcategoria = obtenerSubcategorias("Entrada");
+    expect(Array.isArray(subcategoria)).toBe(true);
+    expect(subcategoria.length).toBeGreaterThan(0);
+  });
+})
+describe('busqueda', () => {
+  it('verificamos que sea funcion', () => {
+    expect(typeof busqueda).toBe('function');
+  });
+  it('devuelve elementos filtrados según el texto de búsqueda', () => {
+    const searchText = 'keyword';
+    const filteredItems = busqueda(searchText);
+    expect(Array.isArray(filteredItems)).toBe(true);
+  });
+});
+describe('filtrado', () => {
+  it('verificamos que sea funcion', () => {
+    expect(typeof filtrado).toBe('function');
+  });
+  it('devuelve productos  filtrados por subcategoria', () => {
+    const producto ="Panadería";
+    const filtradoproducto = filtrado(producto)
+    expect(Array.isArray(filtradoproducto)).toBe(true);
+    expect(filtradoproducto.length).toEqual(7)
+  });
+  it('devuelve productos  filtrados por subcategoria', () => {
+    const producto ="noexiste";
+    const filtradoproducto = filtrado(producto)
+    expect(filtradoproducto.length).toEqual(0);
   });
 });
 
-
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
-
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
-});
